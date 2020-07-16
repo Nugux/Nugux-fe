c71337adcefcc5d9afcc74f5fdf950bf3b22b7ea
90byte <uiop0103@gmail.com>
2020-07-16 22:29:26 +0900

    calculate bounds

--- src/components/PlaceMap/PlaceMap.js
+++ src/components/PlaceMap/PlaceMap.js
@@ -10,10 +10,30 @@ const defaultCenter = [37.5647689, 126.7093638];
 // TODO: 지도 확대/축소 버튼 추가하기
 function PlaceMap() {
   const [location, setLocation] = useState(defaultCenter);
+  const [bounds, setBounds] = useState({ne: {lat:0, lng:0}, sw: {lat:0, lng:0}});
   const [zoomLevel, setZoomLevel] = useState(12);
+  const [api, setApi] = useState(null);
+
+  const calcBounds = () => {
+    if(api) {
+      const bound = api.getBounds();
+      const [sw, ne] = [bound.getNorthEast(), bound.getSouthWest()];
+      setBounds({
+        sw: {
+          lat:sw.lat(),
+          lng:sw.lng()
+        },
+        ne: {
+          lat:ne.lat(),
+          lng:ne.lng()
+        },
+      })
+    }
+  };
 
   const handleDragEnd = ({ center }) => {
     setLocation([center.lat(), center.lng()]);
+    calcBounds()
   };
 
   const handleSelectDate = (date) => {
@@ -24,10 +44,11 @@ function PlaceMap() {
 
   const handleZoom = zoom => {
     // TODO: 특정 레벨 이하로 가는거 막기
-    if (zoom < 100) {
+    if (zoom < 7) {
       return;
     }
     setZoomLevel(zoom);
+    calcBounds();
   };
 
   return (
@@ -39,6 +60,7 @@ function PlaceMap() {
         zoom={zoomLevel}
         onDragEnd={handleDragEnd}
         onZoomAnimationEnd={handleZoom}
+        onGoogleApiLoaded={({map})=>{setApi(map)}}
       />
       <MapHeader handleSelectDate={handleSelectDate} />
     </div>

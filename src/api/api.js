fae3bccbcc312fbb4c25b06f5ea7ef69140795e1
90byte <uiop0103@gmail.com>
2020-07-18 15:20:35 +0900

    add api

--- src/api/api.js
+++ src/api/api.js
@@ -20,36 +20,63 @@ const convSpotLevel = (level) => {
         return 'SPOT'
     }
 };
-
 export const getDailyCongestion = (date, northEast, southWest, spotLevel, callback) => {
+    const translated = convSpotLevel(spotLevel);
+    (translated === 'SPOT')?
+        getDailyCongestionOfCity(date, northEast, southWest, translated, callback)
+        :getTouristSpotInfo(date, northEast, southWest, callback)
+};
+const onSuccessAction = (callback) => {
+    return res => {
+        if (res.status !== 200) {
+            callback({
+                result: null, error: {
+                    status: res.status
+                }
+            })
+        } else {
+            callback({result: res.json(), error: null})
+        }
+    }
+};
+const onErrorAction = (callback) => {
+    return err => {
+        callback({result: null, error: err})
+    }
+};
+
+const sharedOpt = {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer', // no-referrer, *client
+};
+
+const getDailyCongestionOfCity = (date, northEast, southWest, spotLevel, callback) => {
     const params = {
         date: date,
         northEastLat: northEast.lat,
         northEastLong: northEast.lng,
         southWestLat: southWest.lat,
         southWestLong: southWest.lng,
-        spotLevel: convSpotLevel(spotLevel)
+        spotLevel: spotLevel
     };
-    fetch(`${prefix}${urls.getDailyCongestion}?${zipQueryString(params)}`, {
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrer: 'no-referrer', // no-referrer, *client
-    }).then(res => {
-            if (res.status !== 200) {
-                callback({
-                    result: null, error: {
-                        status: res.status
-                    }
-                })
-            } else {
-                callback({result: res.json(), error: null})
-            }
-        },
-        err => {
-            callback({result: null, error: err})
-        })
+    fetch(`${process.env.REACT_APP_SERVER}${prefix}${urls.getDailyCongestion}?${zipQueryString(params)}`, sharedOpt)
+        .then(onSuccessAction(callback),
+            onErrorAction(callback))
+};
+const getTouristSpotInfo = (date, northEast, southWest, callback) => {
+    const params = {
+        date: date,
+        northEastLat: northEast.lat,
+        northEastLong: northEast.lng,
+        southWestLat: southWest.lat,
+        southWestLong: southWest.lng,
+    };
+    fetch(`${process.env.REACT_APP_SERVER}${prefix}${urls.getTouristSpotInfo}?${zipQueryString(params)}`, sharedOpt)
+        .then(onSuccessAction(callback),
+            onErrorAction(callback))
 };

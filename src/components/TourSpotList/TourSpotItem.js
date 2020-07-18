84aa87320fbc0352b0c2f796da49fd842da4d9b2
cutelee <soyoung.dev@gmail.com>
2020-07-18 20:33:39 +0900

    TourSpotItem - 클릭 시 상세정보 표시
    컨텍스트 겁나 잘못썼군여 ㅋ크크크크크... 구조 망했고~

--- src/components/TourSpotList/TourSpotItem.js
+++ src/components/TourSpotList/TourSpotItem.js
@@ -3,10 +3,27 @@ import { List } from "antd";
 import { RightCircleOutlined } from "@ant-design/icons";
 
 import { getCongestionColor } from "../../utils/getColor";
+import { useSidebar } from "../../contexts/sidebar-context";
+import { usePlaceInfo } from "../../contexts/place-info-context";
+import { getTouristSpotDetail } from "../../api/api";
 
 function TourSpotItem({ title, congestion, description, id, shortDesc }) {
+  const [, sidebarDispatch] = useSidebar();
+  const [, placeInfoDispatch] = usePlaceInfo();
   return (
-    <List.Item key={title}>
+    <List.Item
+      key={title}
+      onClick={async () => {
+        sidebarDispatch({ type: "close" });
+        const result = await getTouristSpotDetail(id);
+        placeInfoDispatch({
+          type: "fetch",
+          payload: {
+            id,
+            placeInfo: result,
+          }
+        });
+      }}>
       <List.Item.Meta
         title={(
           <span className="item-title">

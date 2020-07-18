5eeb0cadbfe24807a9a1814c7d53bbae4594ee28
cutelee <soyoung.dev@gmail.com>
2020-07-18 20:23:31 +0900

    TourSpotList - 리스트 스타일 수정

--- src/components/TourSpotList/TourSpotItem.js
+++ src/components/TourSpotList/TourSpotItem.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { List } from "antd";
+import { RightCircleOutlined } from "@ant-design/icons";
 
-function TourSpotItem({ title, congestion, description }) {
+function TourSpotItem({ title, congestion, description, id, shortDesc }) {
   return (
     <List.Item key={title}>
       <List.Item.Meta
-        title={<span>{title}</span>}
+        title={(
+          <span className="item-title">
+            <RightCircleOutlined /> {title}
+          </span>
+        )}
         description={description} />
-      <span>혼잡도 : {congestion}</span>
+      <span className="item-description">{shortDesc}...</span>
     </List.Item>
   );
 }

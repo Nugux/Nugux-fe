aff0735575a7f4f6866f09abefaefd969d040744
cutelee <soyoung.dev@gmail.com>
2020-07-18 17:25:26 +0900

    TourSpotList - 리스트 타이틀 추가 및 스타일 수정

--- src/components/TourSpotList/TourSpotList.js
+++ src/components/TourSpotList/TourSpotList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { List } from "antd";
 import { Swipeable } from "react-swipeable";
+import { BarsOutlined } from "@ant-design/icons";
 
 import { usePlaceList } from "../../contexts/place-list-context";
 import { useSidebar } from "../../contexts/sidebar-context";
 import TourSpotItem from "./TourSpotItem";
 
-import "./TourSpotList.css";
+import "./TourSpotList.scss";
 
 function TourSpotList() {
     const [placeListState] = usePlaceList();
@@ -17,6 +18,10 @@ function TourSpotList() {
             className={'tour-spot-list-view'}
             onSwipedLeft={() => { sidebarDispatch({ type: "close" }); }}
         >
+            <h2 className="title">
+                <BarsOutlined style={{ marginRight: '7px' }} />
+                주변 관광지
+            </h2>
             <List
                 itemLayout="vertical"
                 size="large"

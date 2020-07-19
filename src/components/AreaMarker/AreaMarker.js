80c852ad98feb2fe98be6362cf40604f4cf5b09d
90byte <uiop0103@gmail.com>
2020-07-19 13:23:25 +0900

    fix duplicated named city

--- src/components/AreaMarker/AreaMarker.js
+++ src/components/AreaMarker/AreaMarker.js
@@ -3,7 +3,7 @@ import React, {useEffect} from "react";
 import { useMapLocation } from "../../contexts/place-map-context";
 
 import "./AreaMarker.scss";
-import {geojson} from "res/geojson";
+import {geojson, isDuplicate} from "res/geojson";
 
 const __STATE_KEY = 'CTP_KOR_NM';
 const __CITY_KEY = 'SIG_KOR_NM';
@@ -23,7 +23,7 @@ const setCallback = (name, value) => {
 };
 
 export const commonStyleDrawer = (feature) => {
-    const key = feature.getProperty(__STATE_KEY) || feature.getProperty(__CITY_KEY);
+    const key = feature.getProperty(__CITY_KEY) || feature.getProperty(__STATE_KEY);
     if(__styleDrawerTable[key]) {
         return __styleDrawerTable[key]();
     } else {
@@ -33,7 +33,7 @@ export const commonStyleDrawer = (feature) => {
 
 export const commonOnClickCallback = (evt) => {
     const {feature} = evt;
-    const key = feature.getProperty(__STATE_KEY) || feature.getProperty(__CITY_KEY);
+    const key = feature.getProperty(__CITY_KEY) || feature.getProperty(__STATE_KEY);
     if(__onClickCallbackTable[key]) {
         return __onClickCallbackTable[key]();
     } else {
@@ -41,43 +41,65 @@ export const commonOnClickCallback = (evt) => {
     }
 };
 
-export const AreaMarker = ({ api, congestion, title, lat, long, level }) => {
+export const AreaMarker = ({ api, congestion, title, lat, long, level, state}) => {
     const [, mapLocationDispatch] = useMapLocation();
     const handleClick = async () => {
         const zoomLevel = (level === 'STATE') ? 11 : 15;
         mapLocationDispatch({type: 'location', location: {lat: lat, lng: long}, zoomLevel: zoomLevel})
     };
 
-    const json = (level === 'STATE')?geojson.state:geojson.city;
+    let json = {};
+
+    if (level === 'STATE') {
+        json = geojson.state
+    } else{
+        if(isDuplicate(title)) {
+            json = geojson.city_duplicated;
+        } else {
+            json = geojson.city;
+        }
+    }
 
     let features = null;
 
     setStyleDrawer(title, () => {
         let color = '';
+        let color2 = '';
+        let color3 = '';
         if (congestion < 2.0) {
-            color = 'green';
+            color = '#00aa00';
+            color2 = '#00dd00';
         } else if (congestion < 3.5) {
-            color = 'yellow';
+            color = '#aaaa00';
+            color2 = '#dddd00';
         } else {
-            color = 'red';
+            color = '#aa0000';
+            color2 = '#dd0000';
         }
         return {
             fillColor: color,
             strokeWeight: 2,
+            clickable: true
         }
     });
 
     setCallback(title, handleClick);
 
     const constructBoundary = () => {
-        features = api.data.addGeoJson(json[title], {
+        let gj = null;
+        if(isDuplicate(title) && json[state] && json[state][title]) {
+            gj = json[state][title]
+        } else {
+            gj = json[title];
+        }
+        features = api.data.addGeoJson(gj, {
             idPropertyName:title
         })
     };
 
     const destructBoundary = () => {
         api.data.forEach((feature)=> {
-            const key = feature.getProperty(__STATE_KEY) || feature.getProperty(__CITY_KEY);
+            const key = feature.getProperty(__CITY_KEY) || feature.getProperty(__STATE_KEY);
             if(key === title) {
                 api.data.remove(feature);
             }
